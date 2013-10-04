999937dcad8c890c6a1bb36691a37bb0c8f1260c
tblomseth <thomas@blomseth.dk>
2013-10-04 23:20:36 +0200

    Implemented AccelStepper

--- src/FlappingMaching.ino
+++ src/FlappingMaching.ino
@@ -24,117 +24,190 @@ int fsmState;
 Me_BaseShield baseShield;
 Me_InfraredReceiver infraredReceiver; /* Note: As the code need to use interrupt 0, it works only when the Me - Infrared Receiver module is connected to the port 4(PORT_4) of Me - Base Shield. */
 Me_UltrasonicSensor ultraSensor( PORT_5 ); //Ultrasonic module can ONLY be connected to port 3, 4, 5, 6, 7, 8 of base shield. ultraSensor.distanceCm()
-MakeblockStepper moveItemStepper( 3200, baseShield, PORT_3 );
+// MakeblockStepper moveItemStepper( 3200, baseShield, PORT_3 );
 Me_ServoDriver servoDriver( PORT_2 ); //can ONLY be PORT_1,PORT_2
+AccelStepper moveItemStepper( forwardstep, backwardstep );
+boolean moveItemStepperIsRunning = false;
 
+/* Flapping servo */
 int SERVO1_UP = 130;
 int SERVO1_DOWN = 115;
+
+/* Move item stepper */
+int portNo = PORT_3;
+int pulseLength = 30;
 
 void setup() {
   // Initailize FSM
   fsmState = STATE_OFF;
   
   // Initialize communications
-  Serial.begin(9600);
+  Serial.begin( 9600 );
 
   // Initialize components
   baseShield.begin();  
-  infraredReceiver.begin();  
+  //infraredReceiver.begin();  
   ultraSensor.begin();
-  moveItemStepper.setSpeed( 12 );
+
+  //moveItemStepper.setSpeed( 12 );
+  moveItemStepper.setMaxSpeed( 6400.0 );
+  moveItemStepper.setAcceleration( 38100.0 );
+
   servoDriver.Servo1_begin();
+
+  /* Servo test */
   servoDriver.writeServo1( SERVO1_UP );
   delay( 500 );
   servoDriver.writeServo1( SERVO1_DOWN );
   delay( 500 );
-  servoDriver.writeServo1( SERVO1_UP );  
+  servoDriver.writeServo1( SERVO1_UP ); 
+   
+  /* Move item stepper test */
+  
+  moveItemStepper.runToNewPosition( 800 );
+  
+  Serial.print( "setup: stopMicroSwitch = ");
+  Serial.println( baseShield.readMePortOutsidePin( PORT_8 ) );
+  
 }
 
 void loop() {
-  int remoteKey = infraredReceiver.read();
-  int usDistance = ultraSensor.distanceCm();
+  //int remoteKey = infraredReceiver.read();
+  int remoteKey = -1;
+  int usDistance = 100;
   int stopMicroSwitch = baseShield.readMePortOutsidePin( PORT_8 ); 
-
+/*
+      Serial.println( "usDistance = ");
+      Serial.println( usDistance );
+delay( 500 );
+  */
+  
   switch ( fsmState ) {
     case STATE_OFF:
       Serial.println( "OFF" ); 
       if ( remoteKey == IR_POWER_BUTTON ) {
         fsmState = STATE_WAIT_FOR_ITEM;          
       }
+      fsmState = STATE_WAIT_FOR_ITEM;
       break;
       
     case STATE_WAIT_FOR_ITEM:
       Serial.println( "WAIT FOR ITEM" );
-      Serial.println( "usDistance = ");
-      Serial.println( usDistance );
-      Serial.println( "stopMicroSwitch = ");
-      Serial.println( stopMicroSwitch );
+      usDistance = ultraSensor.distanceCm();
+      Serial.print( "usDistance = "); Serial.println( usDistance );
       delay( 200 );
       if ( usDistance < 10 && stopMicroSwitch == HIGH ) {
-        moveItemStepper.step( 100 );
         fsmState = STATE_MOVE_ITEM_IN;
+        Serial.println( "->> MOVE ITEM IN" );
       }
       break;
       
     case STATE_MOVE_ITEM_IN:
-      Serial.println( "MOVE ITEM IN" );
-      Serial.println( "usDistance = ");
-      Serial.println( usDistance );
-      Serial.println( "stopMicroSwitch = ");
-      Serial.println( stopMicroSwitch );
-      if ( stopMicroSwitch == HIGH ) {
-        moveItemStepper.step( 100 );
+    /*
+      Serial.print( "stopMicroSwitch = "); Serial.println( stopMicroSwitch );
+      Serial.print( "distanceToGo = "); Serial.println( moveItemStepper.distanceToGo() );
+      Serial.print( "speed = "); Serial.println( moveItemStepper.speed() );*/
+      if ( stopMicroSwitch == HIGH && moveItemStepper.distanceToGo() == 0 ) {
+        //moveItemStepper.step( 100 );
+        moveItemStepper.move( 10000 );
+        Serial.print( "targetPosition = "); Serial.println( moveItemStepper.targetPosition() );
+        fsmState = STATE_MOVE_ITEM_IN;
+      } else if ( stopMicroSwitch == HIGH && moveItemStepper.distanceToGo() > 0 ) {
+        moveItemStepper.run();
         fsmState = STATE_MOVE_ITEM_IN;
       } else if ( stopMicroSwitch == LOW ) {
+        moveItemStepper.stop();
+        moveItemStepper.move( 0 );
+        moveItemStepperIsRunning = false;
         fsmState = STATE_PULL_BACK_FLAP;
+        Serial.println( "->> PULL BACK FLAP" );
+        Serial.print( "distanceToGo = "); Serial.println( moveItemStepper.distanceToGo() );
+        Serial.print( "targetPosition = "); Serial.println( moveItemStepper.targetPosition() );
+        //delay( 1000 );
       }
       break;
       
     case STATE_PULL_BACK_FLAP: 
-      Serial.println( "PULL BACK FLAP" );
-      if ( stopMicroSwitch == LOW ) {
-        moveItemStepper.step( 300 );
+      //Serial.println( "PULL BACK FLAP" );
+      if ( moveItemStepper.distanceToGo() == 0 && !moveItemStepperIsRunning ) { 
+        moveItemStepper.move( 700 );
+        Serial.print( "distanceToGo = "); Serial.println( moveItemStepper.distanceToGo() );
+        Serial.print( "targetPosition = "); Serial.println( moveItemStepper.targetPosition() );
+        moveItemStepperIsRunning = true;
+        fsmState = STATE_PULL_BACK_FLAP;
+      } else if ( moveItemStepper.distanceToGo() == 0 && moveItemStepperIsRunning ) {
+        moveItemStepperIsRunning = false;
         fsmState = STATE_LOWER_TOP_FLAP;
-      } 
+        Serial.println( "->> LOWER TOP FLAP" );
+//delay( 1000 );        
+      } else {
+        moveItemStepper.run();
+        fsmState = STATE_PULL_BACK_FLAP;
+      }
       break;
     
     case STATE_LOWER_TOP_FLAP:
       Serial.println( "LOWER TOP FLAP" );    
       servoDriver.writeServo1( SERVO1_DOWN );
-      delay( 1000 );
-      fsmState = STATE_RELEASE_FLAP; 
+      delay( 400 );
+      fsmState = STATE_RELEASE_FLAP;
+      //delay( 1000 );
+      Serial.println( "->> RELEASE FLAP" ); 
     break;  
     
     case STATE_RELEASE_FLAP: 
-      Serial.println( "RELEASE FLAP" );
-      moveItemStepper.step( -300 );
-      fsmState = STATE_RAISE_TOP_FLAP;    
+      if ( moveItemStepper.distanceToGo() == 0 && !moveItemStepperIsRunning ) { 
+        //moveItemStepper.move( -600 );
+        moveItemStepperIsRunning = true;
+        fsmState = STATE_RELEASE_FLAP;        
+      } else if ( moveItemStepper.distanceToGo() == 0 && moveItemStepperIsRunning ) {
+        moveItemStepperIsRunning = false;
+        fsmState = STATE_RAISE_TOP_FLAP;
+ //delay( 1000 );       
+      } else {
+        moveItemStepper.run();
+        fsmState = STATE_RELEASE_FLAP;
+      }
       break;
     
     case STATE_RAISE_TOP_FLAP: 
       Serial.println( "RAISE TOP FLAP" );
       servoDriver.writeServo1( SERVO1_UP );
       fsmState = STATE_MOVE_ITEM_OUT_PHASE_ONE;
+      //delay( 1000 );
+      Serial.println( "->> MOVE ITEM OUT PHASE ONE" );
       break;
 
     case STATE_MOVE_ITEM_OUT_PHASE_ONE: 
-      Serial.println( "MOVE ITEM OUT PHASE ONE" );
-      if ( usDistance > 10 ) {
-        moveItemStepper.step( -600 );
+
+      //usDistance = ultraSensor.distanceCm();
+      usDistance = -1;
+      if ( stopMicroSwitch == LOW && moveItemStepper.distanceToGo() == 0 ) {
+        moveItemStepper.move( -4800 );
+        moveItemStepperIsRunning = true;
         fsmState = STATE_MOVE_ITEM_OUT_PHASE_ONE;
+      } else if ( moveItemStepper.distanceToGo() != 0) {
+        moveItemStepper.run();
+        fsmState = STATE_MOVE_ITEM_OUT_PHASE_ONE;
+      /*} else if( usDistance < 10 && moveItemStepperIsRunning == true ) {
+        moveItemStepper.run();
+        fsmState = STATE_MOVE_ITEM_OUT_PHASE_ONE;*/
       } else {
+        /*moveItemStepper.stop();
+        moveItemStepperIsRunning = false;*/
         fsmState = STATE_MOVE_ITEM_OUT_PHASE_TWO;
       }
       break;
 
     case STATE_MOVE_ITEM_OUT_PHASE_TWO: 
       Serial.println( "MOVE ITEM OUT PHASE TWO" );
-      if ( usDistance < 10 ) {
-        moveItemStepper.step( -600 );
-        fsmState = STATE_MOVE_ITEM_OUT_PHASE_TWO;
-      } else {
+      //if ( usDistance > 10 ) {
+        //moveItemStepper.step( -600 );
+        //moveItemStepper.move( -600 );
+        //fsmState = STATE_MOVE_ITEM_OUT_PHASE_TWO;
+      //} else {
         fsmState = STATE_WAIT_FOR_ITEM;
-      }
+      //}
       break;
     
     default: 
@@ -142,4 +215,25 @@ void loop() {
       break;
   
   }
+}
+
+void forwardstep() { 
+  baseShield.setMePort(portNo, LOW, HIGH);
+  delayMicroseconds( pulseLength );
+  baseShield.setMePort(portNo, HIGH, HIGH);
+  delayMicroseconds( pulseLength );
+  baseShield.setMePort(portNo, HIGH, LOW);
+  delayMicroseconds( pulseLength );
+  baseShield.setMePort(portNo, LOW, LOW);
+  delayMicroseconds( pulseLength );
+}
+void backwardstep() { 
+  baseShield.setMePort(portNo, LOW, LOW);
+  delayMicroseconds( pulseLength );
+  baseShield.setMePort(portNo, HIGH, LOW);
+  delayMicroseconds( pulseLength );
+  baseShield.setMePort(portNo, HIGH, HIGH);
+  delayMicroseconds( pulseLength );
+  baseShield.setMePort(portNo, LOW, HIGH);
+  delayMicroseconds( pulseLength );
 }
